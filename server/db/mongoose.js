ceb85aa2098bdcf54c97fc8cdbf8e5000c280dcf
unknown <rambothanh1985@gmail.com>
2018-05-30 23:13:50 +0700

    Moi them bien MONGODB_URI

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,10 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+// MONGODB_URI ở heroku, lấy bằng cách sử dụng lệnh
+// heroku addons:create mongolab:sandbox --> Tạo addon mongolab gói miễn phí
+// heroku config --> để lấy đường dẫn mongodb hoặc process.env.MONGODB_URI
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {mongoose};
