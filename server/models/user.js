16737c3f78ec71f44ba3e29950a8ae4eb12674a4
unknown <rambothanh1985@gmail.com>
2018-06-09 21:17:59 +0700

    Them cac loai bien moi truong

--- server/models/user.js
+++ server/models/user.js
@@ -61,7 +61,7 @@ UserSchema.methods.toJSON = function () {
 UserSchema.methods.generateAuthToken = function () {
 	var user = this;
 	var access = 'auth';
-	var token = jwt.sign({_id: user._id.toHexString(), access},'abc123').toString();
+	var token = jwt.sign({_id: user._id.toHexString(), access},process.env.JWT_SECRET).toString();
 
 	//Thêm access và token vào đối tượng
 	//user.tokens.push(access, token); không có tác dụng
@@ -95,7 +95,7 @@ UserSchema.statics.findByToken = function (token) {
 
 	try {
 		//Xác nhận mã token (giải mã)
-		decoded = jwt.verify(token, 'abc123');
+		decoded = jwt.verify(token, process.env.JWT_SECRET);
 	} catch (e) {
 		//Nếu xác nhận token không đúng thì:
 		// return new Promise((resolve, reject) => {
